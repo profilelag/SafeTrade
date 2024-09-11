c2147817abb7dc315ef6e5c4d387ac71414edd58
Nicholas Keller <nkeller@profilelag.org>
2024-09-10 23:15:39 -0400

    Get requests working

--- main.go
+++ main.go
@@ -11,20 +11,24 @@ import (
 	_ "github.com/gorilla/websocket"
 )
 
-func RequestMaker(key string, secret string) func (method string, path string, data []byte) (*http.Request, error) {
-	return func (method string, path string, data []byte) (*http.Request, error) {
-		nonce := fmt.Sprintf("%d", time.Now().UnixNano()/1000000)
-		signature := nonce + key
-		hmac_sign := hmac.New(sha256.New, []byte(signature))
-		hmac_sign.Write([]byte(secret))
-		headers := http.Header{
-			"X-Auth-Apikey": {key},
-			"X-Auth-Nonce": {nonce},
-			"X-Auth-Signature": {hex.EncodeToString(hmac_sign.Sum(nil))},
-			"content-type": {"application/json"},
-		}
-		req, err := http.NewRequest(method, "https://safe.trade/api/v2" + path, nil)
-		req.Header = headers
-		return req, err
+func NewRequest(key string, secret string, method string, path string, data []byte) (*http.Request, error) {
+	nonce := fmt.Sprintf("%d", time.Now().UnixMicro()/1000)
+	signature := nonce + key
+	hmac_sign := hmac.New(sha256.New, []byte(secret))
+	hmac_sign.Write([]byte(signature))
+	headers := http.Header{
+		"X-Auth-Apikey": {key},
+		"X-Auth-Nonce": {nonce},
+		"X-Auth-Signature": {hex.EncodeToString(hmac_sign.Sum(nil))},
+		"content-type": {"application/json"},
+	}
+	req, err := http.NewRequest(method, "https://safe.trade/api/v2"+path, nil)
+	req.Header = headers
+	return req, err
+}
+
+func RequestMaker(key string, secret string) func(method string, path string, data []byte) (*http.Request, error) {
+	return func(method string, path string, data []byte) (*http.Request, error) {
+		return NewRequest(key, secret, method, path, data)
 	}
 }
