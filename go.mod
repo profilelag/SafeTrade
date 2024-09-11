58b8533de5b87d29c052d0516fbd155949851c89
Nicholas Keller <nkeller@profilelag.org>
2024-09-10 22:08:31 -0400

    Create RequestMaker

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/profilelag/SafeTrade
 
 go 1.23.0
 
-require github.com/gorilla/websocket v1.5.3 // indirect
+require github.com/gorilla/websocket v1.5.3
---
+++ main.go
@@ -0,0 +1,30 @@
+package safetrade
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"net/http"
+	"time"
+
+	_ "github.com/gorilla/websocket"
+)
+
+func RequestMaker(key string, secret string) func (method string, path string, data []byte) (*http.Request, error) {
+	return func (method string, path string, data []byte) (*http.Request, error) {
+		nonce := fmt.Sprintf("%d", time.Now().UnixNano()/1000000)
+		signature := nonce + key
+		hmac_sign := hmac.New(sha256.New, []byte(signature))
+		hmac_sign.Write([]byte(secret))
+		headers := http.Header{
+			"X-Auth-Apikey": {key},
+			"X-Auth-Nonce": {nonce},
+			"X-Auth-Signature": {hex.EncodeToString(hmac_sign.Sum(nil))},
+			"content-type": {"application/json"},
+		}
+		req, err := http.NewRequest(method, path, nil)
+		req.Header = headers
+		return req, err
+	}
+}
